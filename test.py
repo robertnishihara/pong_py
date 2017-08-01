16ef028a14f030ddfd31455aae846c658ac42f16
Richard <rliaw@berkeley.edu>
2017-08-01 11:09:17 -0700

    updated animation

--- test.py
+++ test.py
@@ -8,39 +8,38 @@ import matplotlib.pyplot as plt
 game = PongJS()
 terminated = False
 state = game.init()
+animation = True
 
 x = []
 y = []
 
-# fig = plt.figure()
-
-# ax = fig.add_subplot(111)
-# line1, = ax.plot(x, y, 'r-')
-plt.ion()
-plt.figure()
-plt.show()
-plt.pause(0.05)
+if animation:
+    plt.ion()
+    plt.figure()
+    plt.show()
+    plt.pause(0.05)
 step = 0
 
 while not terminated:
     step += 1
     ns, r, terminated = game.step(0)
-    ## CHEAT
-    game.left_pad.set_position(game.left_pad.x, ns[1])
-    ##
-    x.append(ns[0])
-    y.append(ns[1]) 
-    print(ns[0], ns[1])
-    # plt.plot(game.left_pad.x, game.left_pad.y, 'o')
-    plt.plot(game.right_pad.x, game.right_pad.y, 'o')
-    if step % 10 == 0:
-        for line in plt.axes().lines:
-            line.remove()
-        plt.plot(x, y)
-        plt.draw()
-        plt.pause(0.05)
-    # time.sleep(0.01)
 
-# fig = plt.figure()
-# plt.plot(x, y)
-# plt.show()
+    #game.step(ACTION)
+
+    ###### CHEAT #######
+    game.left_pad.set_position(game.left_pad.x, ns[1]) 
+    ####################
+
+    if animation:
+        x.append(ns[0])
+        y.append(ns[1]) 
+        print(ns[0], ns[1])
+
+        plt.plot([game.right_pad.x, game.left_pad.x], 
+                [game.right_pad.y, game.left_pad.y], 'o')
+        if step % 5 == 0:
+            for line in plt.axes().lines:
+                line.remove()
+            plt.plot(x, y, c='black')
+            plt.draw()
+            plt.pause(0.01)
